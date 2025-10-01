da2efecdb0ba68d5f7883b4335b02f5140ba3225
Georgii Zhabieiev <georgii.zhabieiev@anyclip.com>
2025-10-01 14:51:34 +0200

    homework for lesson 3

---
+++ lesson3/src/arifmetics.js
@@ -0,0 +1,12 @@
+const x = 10;
+const y = 3;
+const str2 = 'Lesson3';
+
+console.log('a + b =', x + y);
+console.log('a - b =', x - y);
+console.log('a * b =', x * y);
+console.log('a / b =', x / y);
+
+console.log('a + str1 =', x + str2);
+console.log('a * str1 =', x * str2);
+console.log('str2 + b =', str2 + y);
---
+++ lesson3/src/logical.js
@@ -0,0 +1,13 @@
+const num = 10;
+const text = '10';
+const isActive = true;
+
+console.log('num == text:', num == text);
+console.log('num === text:', num === text);
+console.log('num > 5:', num > 5);
+console.log('num <= 10:', num <= 10);
+
+
+console.log('isActive && num > 5:', isActive && num > 5);
+console.log('isActive || num < 5:', isActive || num < 5);
+console.log('!isActive:', !isActive);
