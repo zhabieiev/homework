c90be679cb52b4c7abebfc9482878986daab0f05
Georgii Zhabieiev <georgii.zhabieiev@anyclip.com>
2025-12-10 12:22:52 +0100

    update workflows config

--- lesson22/playwright.config.ts
+++ lesson22/playwright.config.ts
@@ -18,25 +18,44 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 1 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 1 : 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: process.env.CI
+        ? [
+            ['list'],
+            ['blob'],
+            ['allure-playwright'],
+            ['json', { outputFile: 'test-results/test-results.json' }],
+            ['junit', { outputFile: 'test-results/test-results.xml' }],
+            ['pwmochawesome', { charts: true, reportTitle: 'Automation Exercise Test Report', reportFilename: 'automation-exercise-report' }]
+        ]
+        : [
+            ['list']
+        ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'on-first-retry'
+        trace: 'retain-on-failure'
     },
 
     /* Configure projects for major browsers */
     projects: [
         {
             name: 'chromium',
-            use: { ...devices['Desktop Chrome'], headless: false }
+            use: {
+                ...devices['Desktop Chrome'],
+                headless: true,
+                viewport: {
+                    width: 1680,
+                    height: 900
+                }
+                // storageState: '.auth/storage-state-0.json'
+            }
         }
 
         // {
@@ -59,15 +78,15 @@ export default defineConfig({
         //   use: { ...devices['iPhone 12'] },
         // },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
     ]
 
     /* Run your local dev server before starting the tests */
