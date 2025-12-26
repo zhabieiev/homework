2a9ebe95178951f730dc0b13e02366559231f5fa
Georgii Zhabieiev <georgii.zhabieiev@anyclip.com>
2025-12-26 16:55:17 +0100

    update POM and world

--- final/src/support/worlds/world.ts
+++ final/src/support/worlds/world.ts
@@ -1,23 +1,13 @@
-import { World, IWorldOptions, setWorldConstructor } from '@cucumber/cucumber';
+import { World, IWorldOptions, setWorldConstructor, setDefaultTimeout } from '@cucumber/cucumber';
 import { APIRequestContext, APIResponse, Page, Browser, BrowserContext } from '@playwright/test';
 import { TmPlaylistService } from '../../api/services/player.service.ts';
 import { VariablesController } from '../../api/utils/controllers/variables.controller.ts';
-import { setDefaultTimeout } from '@cucumber/cucumber';
 
-export interface TmWorld extends World {
+import { MainPage } from '../../ui/pages/main.page.ts';
+import { VideoPage } from '../../ui/pages/video.page.ts';
+import { SearchResultsPage } from '../../ui/pages/search-results.page.ts';
 
-    browser: Browser;
-    context: BrowserContext;
-    page: Page;
-
-    apiRequest: APIRequestContext;
-    apiResponse: APIResponse;
-
-    varController: VariablesController;
-    playlistService: TmPlaylistService;
-}
-
-export class CustomWorld extends World implements TmWorld {
+export class CustomWorld extends World {
     public browser!: Browser;
     public context!: BrowserContext;
     public page!: Page;
@@ -26,12 +16,36 @@ export class CustomWorld extends World implements TmWorld {
     public apiResponse!: APIResponse;
 
     public varController: VariablesController;
-    public playlistService!: TmPlaylistService; 
+    public playlistService!: TmPlaylistService;
+
+    private _mainPage?: MainPage;
+    private _videoPage?: VideoPage;
+    private _searchResultsPage?: SearchResultsPage;
 
     constructor(options: IWorldOptions) {
         super(options);
-        
         this.varController = new VariablesController();
+    }
+
+    get mainPage(): MainPage {
+        if (!this._mainPage) {
+            this._mainPage = new MainPage(this.page, this.varController);
+        }
+        return this._mainPage;
+    }
+
+    get videoPage(): VideoPage {
+        if (!this._videoPage) {
+            this._videoPage = new VideoPage(this.page, this.varController);
+        }
+        return this._videoPage;
+    }
+
+    get searchResultsPage(): SearchResultsPage {
+        if (!this._searchResultsPage) {
+        this._searchResultsPage = new SearchResultsPage(this.page, this.varController);
+    }
+    return this._searchResultsPage;
     }
 }
 
--- final/src/ui/steps/actions.steps.ts
+++ final/src/ui/steps/actions.steps.ts
@@ -4,28 +4,24 @@ import { MainPage } from '../pages/main.page.ts';
 import { VideoPage } from '../pages/video.page.ts';
 
 Given('ui user opens {string} page', async function (this: CustomWorld, url: string) {
-    const mainPage = new MainPage(this.page, this.varController);
-    await mainPage.navigate(url);
+    await this.mainPage.navigate(url);
 });
 
 When('ui user searches for {string} on main page', async function (this: CustomWorld, query: string) {
-    const mainPage = new MainPage(this.page, this.varController);
-    await mainPage.searchFor(query);
+    await this.mainPage.searchFor(query);
 });
 
 When('ui user clicks on video number {int} in {string} channel', async function (this: CustomWorld, videoIndex: number, channelName: string) {
-    const mainPage = new MainPage(this.page, this.varController);
-    const channel = await mainPage.getChannelByName(channelName);
+    const channel = await this.mainPage.getChannelByName(channelName);
     await channel.clickVideoByIndex(videoIndex);
 });
 
 When('ui user deep searches for {string} on video page', async function (this: CustomWorld, query: string) {
-    const videoPage = new VideoPage(this.page, this.varController);
-    await videoPage.fillVideoSearch(query);
-    await videoPage.openDeepSearch();
+    await this.videoPage.fillVideoSearch(query);
+    await this.videoPage.openDeepSearch();
 });
 
 When('ui user opens {string} tab in Deep Search panel', async function (this: CustomWorld, tabName: string) {
-    const videoPage = new VideoPage(this.page, this.varController);
-    await videoPage.deepSearch.openTab(tabName);
+    //const videoPage = new VideoPage(this.page, this.varController);
+    await this.videoPage.deepSearch.openTab(tabName);
 });
